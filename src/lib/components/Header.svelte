eca9d321ec2ae081a9779809158ac5e3635a79b3
amishk783 <amishkumar800@gmail.com>
2024-03-07 18:25:24 +0530

    removed bug

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,87 +1,97 @@
 <script>
+
 </script>
 
 <header>
-	<div class="flex flex-col flex-wrap gap-y-2.5 justify-between content-center pt-2.5">
-		<div class="flex gap-5 justify-between w-full max-md:flex-wrap max-md:max-w-full">
-			<div class="max-sm:p-5 flex gap-1 px-px my-auto">
-				<img loading="lazy" alt="logo" src="logo.svg" class="aspect-square w-[27px]" />
-				<img
-					loading="lazy"
-					alt="icon"
-					src="https://cdn.builder.io/api/v1/image/assets/TEMP/537d104be4026a7c56486315b34f70c6dbc34b4b468004932b3228deabcc7545?"
-					class="my-auto aspect-[3.57] fill-zinc-800 w-[76px]"
-				/>
-			</div>
-			<div class="flex gap-5 justify-between max-md:flex-wrap max-md:max-w-full">
-				<div
-					class="hidden md:flex gap-5 justify-between my-auto text-sm leading-5 whitespace-nowrap max-md:flex-wrap max-md:max-w-full"
-				>
-					<div class="flex gap-0 px-5 text-neutral-600">
-						<div class="flex gap-3 self-start px-5 py-2 rounded-[40px]">
-							<img
-								alt="icon"
-								loading="lazy"
-								src="https://cdn.builder.io/api/v1/image/assets/TEMP/c564b3064e2b069dc954a5597b0ba52d67084f050544e84e857e7e6546e79272?"
-								class="my-auto w-4 aspect-square"
-							/>
-							<div>Home</div>
-						</div>
-						<div class="flex gap-1 justify-between px-5 py-2.5 rounded-[40px]">
-							<img
-								alt="icon"
-								loading="lazy"
-								src="https://cdn.builder.io/api/v1/image/assets/TEMP/bb0991df5af0b4281c6d61d4c634aa0f61d4abe099218222a250f6a405c2d8ae?"
-								class="my-auto w-4 aspect-square"
-							/>
-							<div class="grow">Explore</div>
-						</div>
-						<div class="flex gap-3 justify-between px-5 py-2.5">
-							<img
-								alt="icon"
-								loading="lazy"
-								src="https://cdn.builder.io/api/v1/image/assets/TEMP/c0ffd805db9f65369dba115684e271365657a1b2ba2c932bb2932b1dd7e5bbc0?"
-								class="my-auto aspect-[1.08] stroke-[1px] stroke-stone-500 w-[15px]"
-							/>
-							<div>Learn</div>
-						</div>
-					</div>
-					<div
-						class="flex gap-3 self-start px-5 py-2 bg-white rounded border-solid shadow-sm border-[0.5px] border-neutral-600 text-neutral-600"
-					>
-						<img
-							loading="lazy"
-							alt="icon"
-							src="https://cdn.builder.io/api/v1/image/assets/TEMP/e43414bffb7a8b20765446c36ab316aea49cc9a372b6e19d575a6d3ee9818fc9?"
-							class="my-auto w-4 aspect-square"
-						/>
-						<div>Post</div>
-					</div>
-				</div>
-				<div
-					class="flex gap-3 items-center px-4 py-0.5 text-xl font-medium leading-6 text-neutral-600"
-				>
-					<img
-						loading="lazy"
-						src="/profile.png"
-						alt="icon"
-						class="md:self-stretch w-12 aspect-square rounded-full"
-					/>
-					<div class="flex-auto self-stretch my-auto">Krishna Kiran</div>
-					<img
-						loading="lazy"
-						alt="icon"
-						src="https://cdn.builder.io/api/v1/image/assets/TEMP/64575af3d5780ca3732834369870ebe8c92d42faf885c7470467c65080d67b11?"
-						class="self-stretch my-auto w-4 aspect-square"
-					/>
-				</div>
-			</div>
-		</div>
-		<img
-			loading="lazy"
-			alt="icon"
-			src="https://cdn.builder.io/api/v1/image/assets/TEMP/7e687b8919ed73c695330a272480db759d723e82145688c0dfb0e83ddbe8ab84?"
-			class="mt-2.5 w-full max-md:max-w-full"
-		/>
-	</div>
+<div
+  class="flex flex-col flex-wrap gap-y-2.5 justify-between content-center pt-2.5 "
+>
+  <div
+    class="flex gap-5 justify-between w-full max-md:flex-wrap max-md:max-w-full"
+  >
+    <div class="flex gap-1 px-px my-auto">
+      <img
+        loading="lazy"
+        alt="logo"
+        src="logo.svg"
+        class="aspect-square w-[27px]"
+      />
+      <img
+        loading="lazy"
+        alt="icon"
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/537d104be4026a7c56486315b34f70c6dbc34b4b468004932b3228deabcc7545?"
+        class="my-auto aspect-[3.57] fill-zinc-800 w-[76px]"
+      />
+    </div>
+    <div class="flex gap-5 justify-between max-md:flex-wrap max-md:max-w-full">
+      <div
+        class="flex gap-5 justify-between my-auto text-sm leading-5 whitespace-nowrap max-md:flex-wrap max-md:max-w-full"
+      >
+        <div class="flex gap-0 px-5 text-neutral-600">
+          <div class="flex gap-3 self-start px-5 py-2 rounded-[40px]">
+            <img
+            alt="icon"
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/c564b3064e2b069dc954a5597b0ba52d67084f050544e84e857e7e6546e79272?"
+              class="my-auto w-4 aspect-square"
+            />
+            <div>Home</div>
+          </div>
+          <div class="flex gap-1 justify-between px-5 py-2.5 rounded-[40px]">
+            <img
+            alt="icon"
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/bb0991df5af0b4281c6d61d4c634aa0f61d4abe099218222a250f6a405c2d8ae?"
+              class="my-auto w-4 aspect-square"
+            />
+            <div class="grow">Explore</div>
+          </div>
+          <div class="flex gap-3 justify-between px-5 py-2.5">
+            <img
+            alt="icon"
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/c0ffd805db9f65369dba115684e271365657a1b2ba2c932bb2932b1dd7e5bbc0?"
+              class="my-auto aspect-[1.08] stroke-[1px] stroke-stone-500 w-[15px]"
+            />
+            <div>Learn</div>
+          </div>
+        </div>
+        <div
+          class="flex gap-3 self-start px-5 py-2 bg-white rounded border-solid shadow-sm border-[0.5px] border-neutral-600 text-neutral-600"
+        >
+          <img
+            loading="lazy"
+            alt="icon"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/e43414bffb7a8b20765446c36ab316aea49cc9a372b6e19d575a6d3ee9818fc9?"
+            class="my-auto w-4 aspect-square"
+          />
+          <div>Post</div>
+        </div>
+      </div>
+      <div
+        class="flex gap-3 items-center px-4 py-0.5 text-xl font-medium leading-6 text-neutral-600"
+      >
+        <img
+          loading="lazy"
+          src="profile.png"
+          alt="icon"
+          class="self-stretch w-12 aspect-square rounded-full"
+        />
+        <div class="flex-auto self-stretch my-auto">Krishna Kiran</div>
+        <img
+          loading="lazy"
+          alt="icon"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/64575af3d5780ca3732834369870ebe8c92d42faf885c7470467c65080d67b11?"
+          class="self-stretch my-auto w-4 aspect-square"
+        />
+      </div>
+    </div>
+  </div>
+  <img
+    loading="lazy"
+    alt="icon"
+    src="https://cdn.builder.io/api/v1/image/assets/TEMP/7e687b8919ed73c695330a272480db759d723e82145688c0dfb0e83ddbe8ab84?"
+    class="mt-2.5 w-full max-md:max-w-full"
+  />
+</div>
 </header>
